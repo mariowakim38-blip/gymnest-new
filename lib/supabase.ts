12dd781cd5282d1a158317fc541a99ec6daa3bee
mariowakim38-blip <mariowakim38@gmail.com>
2026-04-24 17:13:15 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://znsakxenacqakqtzqmfi.supabase.co"
-const supabaseAnonKey = sb_publishable_YWAuQxlCJ8MMwXf_r38eUw_XzEWjTr6
+const supabaseAnonKey = "sb_publishable_YWAuQxlCJ8MMwXf_r38eUw_XzEWjTr6"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
