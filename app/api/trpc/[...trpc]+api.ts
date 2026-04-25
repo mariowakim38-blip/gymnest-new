837eb966134931d37929fad0e82ed6f5a0c82179
mariowakim38-blip <mariowakim38@gmail.com>
2026-04-25 17:49:13 +0300

    Update [...trpc]+api.ts

--- app/api/trpc/[...trpc]+api.ts
+++ app/api/trpc/[...trpc]+api.ts
@@ -2,12 +2,19 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from '../../../backend/trpc/app-router';
 import { createContext } from '../../../backend/trpc/create-context';
 
-const handler = (req: Request) =>
-  fetchRequestHandler({
+const handler = (req: Request) => {
+  return fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
     createContext,
   });
+};
 
-export { handler as GET, handler as POST };
+export async function GET(req: Request) {
+  return handler(req);
+}
+
+export async function POST(req: Request) {
+  return handler(req);
+}
