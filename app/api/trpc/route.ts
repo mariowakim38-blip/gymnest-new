a562fd0ed5cfb0c5fb3a9b10efafa612e1bfc541
mariowakim38-blip <mariowakim38@gmail.com>
2026-04-25 17:50:18 +0300

    Update route.ts

--- app/api/trpc/route.ts
+++ app/api/trpc/route.ts
@@ -1,15 +1,1 @@
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { appRouter } from "../../../backend/trpc/app-router";
-import { createContext } from "../../../backend/trpc/create-context";
 
-export const dynamic = "force-dynamic";
-
-const handler = (req: Request) =>
-  fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req,
-    router: appRouter,
-    createContext,
-  });
-
-export { handler as GET, handler as POST };
